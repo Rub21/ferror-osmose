da875d8a5dd408e7e2f521f974c9a9609d82492a
Rub21 <ruben@mapbox.com>
2015-03-30 17:57:05 -0500

    fixing

--- filter.sh
+++ filter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -S --progress-bar "http://osmose.openstreetmap.fr/export/osmose-planet-latest.csv.bz2" > osmose-planet-latest.csv.bz2
-gzip -d $name.csv.bz2
+gzip -d osmose-planet-latest.csv.bz2
 mkdir data
 while read line
 do
@@ -9,4 +9,4 @@ do
     eval "csvgrep -c analyser -r \"$n\" osmose-planet-latest.csv  > data/$n.csv"    
     python csv2shp.py $n.csv $n.shp
 done < $1
-rm osmose-planet-latest.csv
+#rm osmose-planet-latest.csv
